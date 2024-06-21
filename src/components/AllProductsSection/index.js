6e9efea5267263ffe1b9c97eb2fbe2290c504f05
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-06-21 16:30:32 +0530

    Update index.js

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -93,7 +93,7 @@ class AllProductsSection extends Component {
     const jwtToken = Cookies.get('jwt_token')
     const {activeOptionId, activeCategoryId, searchInput, activeRatingId} =
       this.state
-    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}`
+    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}&category=${activeCategoryId}&title_search=${searchInput}&rating=${activeRatingId}`
     const options = {
       headers: {
         Authorization: `Bearer ${jwtToken}`,
