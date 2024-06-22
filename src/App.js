483e423589981aaace23f4ab78d43441daffd586
Kamran Ahmad <mohammadkamranahmad786@gmail.com>
2024-06-22 22:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
     <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/product" component={Products} />
     <ProtectedRoute exact path="/cart" component={Cart} />
     <Route path="/not-found" component={NotFound} />
     <Redirect to="not-found" />
